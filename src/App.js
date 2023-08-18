7f0e0b6bee7db21e84b843b9aba9969c1e78e6a3
Karolis Kasparavičius <118920577+karoliskarol@users.noreply.github.com>
2023-08-18 19:02:03 +0300

    Sections
    Added 3 more sections.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ function App() {
     <div className="App">
       <Home />
       <MiddleSection />
+
+      <MiddleSection
+        direction="flex-row-reverse"
+        title="Download your shows to watch offline"
+        subtitle="Save your favorites easily and always have something to watch."
+        src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg"
+      />
+
+      <MiddleSection
+        title="Watch everywhere"
+        subtitle="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+        src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png"
+      />
+
+      <MiddleSection
+        direction="flex-row-reverse"
+        title="Create profiles for kids"
+        subtitle="Send kids on adventures with their favorite characters in a space made just for them—free with your membership."
+        src="https://occ-0-38-768.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABejKYujIIDQciqmGJJ8BtXkYKKTi5jiqexltvN1YmvXYIfX8B9CYwooUSIzOKneblRFthZAFsYLMgKMyNfeHwk16DmEkpIIcb6A3.png?r=f55"
+      />
     </div>
   );
 }
--- src/Components/MiddleSection.js
+++ src/Components/MiddleSection.js
@@ -1,17 +1,29 @@
-const MiddleSection = () => {
+const MiddleSection = props => {
+    props = {
+        ...{
+            direction: 'flex-row',
+            title: 'Enjoy on your TV',
+            subtitle: 'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
+            src: 'https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png'
+        },
+        ...props
+    }
+
     return (
-        <section>
+        <section className="middle-section grey-top-border">
             <div className="container">
-                <div className="row">
-                    <div className="col-6">
-                        <div className="middle-section-title">
-                        Enjoy on your TV
+                <div className={"row " + props.direction}>
+                    <div className="col-6 d-flex justify-content-center flex-column">
+                        <div className="middle-section-title mb-2">
+                            {props.title}
                         </div>
                         <div className="middle-section-title-2">
-                        Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.
+                            {props.subtitle}
                         </div>
                     </div>
-                    <div className="col-6">d</div>
+                    <div className="col-6">
+                        <img src={props.src} />
+                    </div>
                 </div>
             </div>
         </section>
